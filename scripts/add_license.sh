162d5f8a2143c9038197d26d47aebe6f2bd11369
hayzamjs <hayzam@alchemilla.io>
2025-04-01 00:08:31 +0400

    chore: update license in files

--- scripts/add_license.sh
+++ scripts/add_license.sh
@@ -2,4 +2,4 @@
 
 addlicense -f docs/CODE_LICENSE \
   -ignore internal/assets/web-files/** \
-  cmd/** internal/** web/src/lib/**/*.ts web/src/lib/*.ts
+  cmd/** internal/** web/src/lib/**/*.ts web/src/lib/*.ts pkg/utils/** pkg/crypto/** pkg/disk/**

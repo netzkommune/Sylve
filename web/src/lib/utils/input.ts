08eb6259a1064b63f910d3417dbdf8174976db7c
hayzamjs <hayzam@alchemilla.io>
2025-05-26 14:58:41 +0400

    chore: add license to new files

--- web/src/lib/utils/input.ts
+++ web/src/lib/utils/input.ts
@@ -1,3 +1,13 @@
+/**
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2025 The FreeBSD Foundation.
+ *
+ * This software was developed by Hayzam Sherif <hayzam@alchemilla.io>
+ * of Alchemilla Ventures Pvt. Ltd. <hello@alchemilla.io>,
+ * under sponsorship from the FreeBSD Foundation.
+ */
+
 export function generateComboboxOptions(values: string[], additional?: string[]) {
 	const combined = [...values, ...(additional ?? [])];
 	return combined.map((option) => ({

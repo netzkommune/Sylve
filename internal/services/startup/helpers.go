dad5f396baac5f12703abcb083603b808bca41ed
hayzamjs <hayzam@alchemilla.io>
2025-08-14 20:03:01 +0400

    chore: update licenses

--- internal/services/startup/helpers.go
+++ internal/services/startup/helpers.go
@@ -1,3 +1,11 @@
+// SPDX-License-Identifier: BSD-2-Clause
+//
+// Copyright (c) 2025 The FreeBSD Foundation.
+//
+// This software was developed by Hayzam Sherif <hayzam@alchemilla.io>
+// of Alchemilla Ventures Pvt. Ltd. <hello@alchemilla.io>,
+// under sponsorship from the FreeBSD Foundation.
+
 package startup
 
 import (
--- web/src/lib/utils/explorer.ts
+++ web/src/lib/utils/explorer.ts
@@ -1,3 +1,13 @@
+/**
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2025 The FreeBSD Foundation.
+ *
+ * This software was developed by Hayzam Sherif <hayzam@alchemilla.io>
+ * of Alchemilla Ventures Pvt. Ltd. <hello@alchemilla.io>,
+ * under sponsorship from the FreeBSD Foundation.
+ */
+
 import type { FileNode } from '$lib/types/system/file-explorer';
 
 export type SortBy = 'name-asc' | 'name-desc' | 'modified-asc' | 'modified-desc' | 'size-desc' | 'type';

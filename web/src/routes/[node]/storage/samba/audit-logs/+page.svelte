409a048bb8230327edc661167cc61a72b21356a5
hayzamjs <hayzam@alchemilla.io>
2025-08-19 13:06:13 +0400

    ui: samba: audit-log: fix path parser (due to zfs change)

--- web/src/routes/[node]/storage/samba/audit-logs/+page.svelte
+++ web/src/routes/[node]/storage/samba/audit-logs/+page.svelte
@@ -20,7 +20,7 @@
 		const row = cell.getRow();
 		const share = data.shares.find((s) => s.name === row.getData().share);
 		if (share) {
-			const dataset = data.datasets.find((d) => d.properties.guid === share.dataset);
+			const dataset = data.datasets.find((d) => d.guid === share.dataset);
 			if (dataset?.mountpoint) {
 				const path = cell.getValue().replace(dataset.mountpoint, '');
 				return path || '-';
